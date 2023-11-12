bce6c985a7721387a5953542e6794676e30432f1
stukafly <ottoace@hotmail.se>
2023-11-12 12:11:18 +0100

    made new rounds and end after three rounds

--- F4 Scoring Sweden padda.py
+++ F4 Scoring Sweden padda.py
@@ -16,7 +16,15 @@ def judges_data():
                     (IDnr int key, judge TEXT, judgenr INT, pilot TEXT, startnr INT, R1M1 INT, R1M2 INT, 
                     R1M3 INT, R1M4 INT, R1M5 INT, R1M6 INT, R1M7 INT, R1M8 INT, R1M9 INT, R1M10 INT, R1M11 INT, 
                     R1M12 INT, R1M13 INT, R1MT1 TEXT, R1MT2 TEXT, R1MT3 TEXT, R1MT4 TEXT, R1MT5 TEXT, R1MT6 TEXT,
-                    R1MT7 TEXT, R1MT8 TEXT, R1MT9 TEXT, R1MT10 TEXT, R1MT11 TEXT, R1MT12 TEXT, R1MT13 TEXT)
+                    R1MT7 TEXT, R1MT8 TEXT, R1MT9 TEXT, R1MT10 TEXT, R1MT11 TEXT, R1MT12 TEXT, R1MT13 TEXT,
+                    R2M1 INT, R2M2 INT, 
+                    R2M3 INT, R2M4 INT, R2M5 INT, R2M6 INT, R2M7 INT, R2M8 INT, R2M9 INT, R2M10 INT, R2M11 INT, 
+                    R2M12 INT, R2M13 INT, R2MT1 TEXT, R2MT2 TEXT, R2MT3 TEXT, R2MT4 TEXT, R2MT5 TEXT, R2MT6 TEXT,
+                    R2MT7 TEXT, R2MT8 TEXT, R2MT9 TEXT, R2MT10 TEXT, R2MT11 TEXT, R2MT12 TEXT, R2MT13 TEXT,
+                    R3M1 INT, R3M2 INT, 
+                    R3M3 INT, R3M4 INT, R3M5 INT, R3M6 INT, R3M7 INT, R3M8 INT, R3M9 INT, R3M10 INT, R3M11 INT, 
+                    R3M12 INT, R3M13 INT, R3MT1 TEXT, R3MT2 TEXT, R3MT3 TEXT, R3MT4 TEXT, R3MT5 TEXT, R3MT6 TEXT,
+                    R3MT7 TEXT, R3MT8 TEXT, R3MT9 TEXT, R3MT10 TEXT, R3MT11 TEXT, R3MT12 TEXT, R3MT13 TEXT)
             '''
         conn.execute(table_create_query)
         c = conn.cursor()
@@ -76,22 +84,39 @@ def startnummer():
      startbutton.destroy()
      startnummerlabel.config(text="Start Nr:")
      startnummerlabel.grid(row=0, column=0)
-     startnummer2label.config(text="1")
+     startnummer2label.config(text=str(startnummerval))
      startnummer2label.grid(row=0, column=1)
      roundnummerlabel.config(text="Round Nr:")
      roundnummerlabel.grid(row=1, column=0)
-     roundnummerlabel2.config(text="2")
+     roundnummerlabel2.config(text=str(roundnr))
      roundnummerlabel2.grid(row=1, column=1)
      startcompetition.config(text="start Round", command=judgepoint)
      startcompetition.grid(row=2, column=2)
 
+     try:
+         global numberofcompetitors
 
+
+         connection = sqlite3.connect('F4scoring.db')
+         cursor = connection.cursor()
+
+         numberofcompetitors_query = """SELECT COUNT(pilot) from judge_data"""
+         cursor.execute(numberofcompetitors_query)
+         numberofcompetitors = cursor.fetchone()[0]
+         cursor.close()
+
+     except sqlite3.Error as error:
+
+         tkinter.messagebox.showwarning(title="Error", message="hittar inte databas")
+     finally:
+         if connection:
+             connection.close()
 
 
 def judgepoint():
     try:
         global manval
-
+        global startnummerval
 
 
         connection = sqlite3.connect('F4scoring.db')
@@ -99,7 +124,7 @@ def judgepoint():
 
         sqlite_select_query = """SELECT * from judge_data"""
         cursor.execute(sqlite_select_query)
-        records = cursor.fetchmany(int(1))
+        records = cursor.fetchmany(int(startnummerval))
 
         for row in records:
             pilot = (row[3])
@@ -121,12 +146,15 @@ def judgepoint():
     roundnummerlabel.grid_forget()
     roundnummerlabel2.grid_forget()
     startcompetition.grid_forget()
-    Stortminus  = tkinter.Button(window, font=('Times', 48))
+
+
+
+    Stortminus.config (font=('Times', 48))
     Stortminus ["bg"] = "#fb5050"
     Stortminus["fg"] = "#000000"
     Stortminus ["justify"] = "center"
     Stortminus ["text"] = "-"
-    Stortminus .place(x=0, y=110, width=312, height=314)
+    Stortminus .place(x=0, y=110, width=300, height=314)
     Stortminus ["command"] = spinminus
 
 
@@ -134,46 +162,47 @@ def judgepoint():
     manresult["borderwidth"] = "1px"
     manresult["fg"] = "#333333"
     manresult["justify"] = "center"
-    manresult.place(x=320, y=110, width=291, height=315)
+    manresult.place(x=320, y=110, width=300, height=314)
 
-    Stortplus = tkinter.Button(window, font=('Times', 48))
+    Stortplus.config(font=('Times', 48))
     Stortplus["bg"] = "#87fb3a"
     Stortplus["fg"] = "#000000"
     Stortplus["justify"] = "center"
     Stortplus["text"] = "+"
-    Stortplus.place(x=620, y=110, width=271, height=312)
+    Stortplus.place(x=630, y=110, width=300, height=314)
     Stortplus["command"] = spinplus
 
-    Next1 = tkinter.Button(window, font=('Times', 48))
+    Next1.config (font=('Times', 48))
     Next1["bg"] = "#87fb3a"
     Next1["fg"] = "#000000"
     Next1["justify"] = "center"
     Next1["text"] = "Next"
-    Next1.place(x=340, y=400, width=180, height=180)
+    Next1.place(x=370, y=420, width=180, height=180)
     Next1["command"] = nextmanover
 
-    GLabel_247 = tkinter.Label(window, font=('Times', 18))
+    GLabel_247.config(font=('Times', 18))
     GLabel_247["fg"] = "#333333"
     GLabel_247["justify"] = "center"
     GLabel_247["text"] = "pilot:"
     GLabel_247.place(x=100, y=20, width=200, height=25)
 
-    startnamn2 = tkinter.Label(window, font=('Times', 18))
+    startnamn2.config(font=('Times', 18))
     startnamn2["fg"] = "#333333"
     startnamn2["justify"] = "left"
     startnamn2["text"] = pilot
     startnamn2.place(x=100, y=50, width=200, height=25)
 
-    GLabel_2 = tkinter.Label(window, font=('Times', 18))
+    GLabel_2.config(font=('Times', 18))
     GLabel_2["fg"] = "#333333"
     GLabel_2["justify"] = "center"
     GLabel_2["text"] = "Manöver:"
     GLabel_2.place(x=350, y=20, width=200, height=25)
 
-    GLabel_437 = tkinter.Label(window, font=('Times', 18), justify="center")
+    GLabel_437.config(font=('Times', 18), justify="center")
     GLabel_437["fg"] = "#333333"
     GLabel_437["text"] = manover
     GLabel_437.place(x=350, y=40, width=200, height=30)
+
 
 
 def spinplus():
@@ -196,18 +225,65 @@ def spinminus  ():
 def nextmanover():
     global manval
     global x
-    man = manval - 17
-    cell = str("R1M"+str(man))
+    global startnummerval
+    global roundnr
+    global newround
+
+    manval1 = manval - newround
+    man = manval1 - 17
+    st = str(startnummerval)
+    cell = str("R"+str(roundnr)+"M"+str(man))
     conn = sqlite3.connect('F4scoring.db')
     c = conn.cursor()
-    c.execute('''UPDATE judge_Data SET  {0}  = ? WHERE startnr = (1)'''.format(cell),[str(x)])
-
-
+    c.execute('''UPDATE judge_Data SET  {0}  = (?) WHERE startnr = {1} '''.format(cell,st),[str(x)])
     conn.commit()
     conn.close()
-
     manval += 1
-    judgepoint()
+
+
+    if man <= 12:
+        judgepoint()
+    else:
+        nextstart()
+
+def nextstart():
+    global manval
+    global startnummerval
+    global roundnr
+    global newround
+    Stortplus.place_forget()
+    Stortminus.place_forget()
+    manresult.place_forget()
+    Next1.place_forget()
+    startnamn2.place_forget()
+    GLabel_247.place_forget()
+    GLabel_2.place_forget()
+    GLabel_437.place_forget()
+
+    manval = 18 + newround
+    startnummerval += 1
+
+    if startnummerval > numberofcompetitors:
+       nyrunda()
+    else:
+        startnummer()
+
+def slut():
+    window.title("tävling slut")
+
+def nyrunda():
+    global startnummerval
+    global roundnr
+    global newround
+    global manval
+    roundnr += 1
+    startnummerval = 1
+    newround += 26
+    manval = 18 + newround
+    if roundnr > 3:
+        slut()
+    else:
+        startnummer()
 
 
 window = tkinter.Tk()
@@ -240,16 +316,22 @@ startnummer2label = tkinter.Label(frame, text="", font=('Arial', 25), width=2)
 roundnummerlabel = tkinter.Label(frame, text="", font=('Arial', 25), width=8)
 roundnummerlabel2 = tkinter.Label(frame, text="", font=('Arial', 25), width=2)
 startcompetition = tkinter.Button(frame, text="", width=20, height=10, font=('Arial', 25))
-Stortplus = tkinter.Button(frame, text="", width=2, height=10, font=('Arial', 25))
-Stortminus = tkinter.Button(frame, text="", width=2, height=10, font=('Arial', 25))
+Stortplus = tkinter.Button(window, text="", width=2, height=10, font=('Arial', 25))
+Stortminus = tkinter.Button(window, text="", width=2, height=10, font=('Arial', 25))
 manresult = tkinter.Label(window,font=('Times', 80))
 startnamn = tkinter.Label(frame, text="", font=('Arial', 25), width=15)
 startnamn1 = tkinter.Label(frame, text="", font=('Arial', 25), width=15)
-Next1 = tkinter.Button(frame, text="", width=2, height=10, font=('Arial', 25))
-startnamn2 = tkinter.Label(frame, text="", font=('Arial', 25), width=15)
+Next1 = tkinter.Button(window, text="", width=2, height=10, font=('Arial', 25))
+startnamn2 = tkinter.Label(window, text="", font=('Arial', 25), width=15)
+GLabel_247 = tkinter.Label(window, font=('Times', 18))
+GLabel_2 = tkinter.Label(window, font=('Times', 18))
+GLabel_437 = tkinter.Label(window, font=('Times', 18), justify="center")
+numberofcompetitors = 1
 manval = 18
 x = float(8.0)
-
+startnummerval = 1
+roundnr = 1
+newround = 0
 
 
 window.mainloop()
